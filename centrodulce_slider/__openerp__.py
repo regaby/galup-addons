2139ba783ac31517d7b3d6f7b75f1bcbd06c2092
Gabriela Rivero <regaby@gmail.com>
2017-11-21 13:24:16 -0300

    [ADD] modules centro_dulce.

---
+++ centrodulce_slider/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# author: 63720750@qq.com
+{
+    'name': 'Centro dulce slider',
+    'description': 'This module provides feature for carousel-thumbnail-indicators theme.',
+    'summary': 'This module provides feature for carousel-thumbnail-indicators theme.',
+    'category': 'Theme',
+    'version': '1.0',
+    'website': 'http://galup.com.ar',
+    'author': 'Galup',
+    'depends': ['website','theme_default'],
+    'data': [
+        'views/theme.xml',
+    ],
+    'images':[
+            'static/description/main_screenshot.jpg',
+    ],
+    'application': True,
+
+    # 'price': 9.99,
+    # 'currency': 'EUR',
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
