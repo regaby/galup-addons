ce6f4ff3bd1d0343d20c23954997521547d7286f
luciano <lucianogenessini@gmail.com>
2018-03-03 12:31:21 -0300

    [UPD] traducciones de genero y estado civil reporte folio

--- galup_hotel/reports/registro_huesped.py
+++ galup_hotel/reports/registro_huesped.py
@@ -11,9 +11,19 @@ from datetime import timedelta, datetime
 
 
 GENDER={'male': 'Masculino','female': 'Femenino','other': 'Otro'}
-MARITAL={'single': 'Soltero'}
+MARITAL={'single':'Soltero/a', 'married':'Casado/a', 'widower':'Viudo/a','divorced':'Divorciado/a'}
 class Parser(report_sxw.rml_parse):
 
     def __init__(self, cr, uid, name, context):
         super(Parser, self).__init__(cr, uid, name, context)
+        self.localcontext.update({
+            'get_gender':self._get_gender,
+            'get_marital':self._get_marital,
+            
+        })
         
+    def _get_gender(self, gender):
+        return GENDER[gender]
+
+    def _get_marital(self, marital):
+        return MARITAL[marital]
