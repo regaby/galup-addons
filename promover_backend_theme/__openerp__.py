5b888c46cb7f87d7408624e5611f1f4f43897674
Gabriela Rivero <regaby@gmail.com>
2018-04-19 00:44:26 -0300

    [ADD] promover_backend_theme

---
+++ promover_backend_theme/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Promover Backend Theme",
+    "summary": "Promover backend theme",
+    "version": "9.0.6",
+    "category": "Themes/Backend",
+    "website": "http://www.galup.com.ar",
+    "description": """
+                Backend theme for Odoo 9.0 Promover.
+    """,
+    'depends': ['base', 'v9c_backend_theme'],
+    'data': ['views.xml',],
+    'application': True,
+    'auto_install': False,
+    'installable': True,
+}
