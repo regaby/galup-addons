211db19d19d024c820ac7e66e18c39c048a88d2b
Gabriela Rivero <regaby@gmail.com>
2018-05-22 18:59:34 -0300

    [UPD] recibo_x para ser imprimido desde linea de pago en folio y reserva.

--- galup_hotel/reports/recibo_x.py
+++ galup_hotel/reports/recibo_x.py
@@ -19,6 +19,7 @@ class Parser(report_sxw.rml_parse):
         super(Parser, self).__init__(cr, uid, name, context)
         self.localcontext.update({
             'amount_to_text':self._amount_to_text,
+            'get_objects':self._get_objects,
             
         })
         
@@ -30,4 +31,44 @@ class Parser(report_sxw.rml_parse):
             amount_in_words += _(' and %s/100') % str(int(round(float_round(decimals*100, precision_rounding=1))))
         return amount_in_words
 
-    
+    def _get_objects(self, objects, context=None):
+        active_id = self.localcontext['active_ids'][0]
+        cr = self.cr
+        uid = self.uid
+        obj = self.pool.get('hotel.payment').browse(cr, uid, active_id)
+        if obj.folio_id:
+            vals = {
+                'website': obj.folio_id.warehouse_id.partner_id.website,
+                'city': obj.folio_id.warehouse_id.partner_id.city,
+                'state_id': obj.folio_id.warehouse_id.partner_id.state_id,
+                'country_id': obj.folio_id.warehouse_id.partner_id.country_id,
+                'partner_id': obj.folio_id.partner_id.name,
+                'partner_street': obj.folio_id.partner_id.street,
+                'partner_city': obj.folio_id.partner_id.city,
+                'partner_state_id': obj.folio_id.partner_id.state_id,
+                'partner_country_id': obj.folio_id.partner_id.country_id,
+                'partner_phone': obj.folio_id.partner_id.phone,
+                'amount_total': obj.amount,
+                'fecha_pago': obj.payment_date,
+                'concepto': 'Pago de alojamiento.-',
+            }
+        else:
+            vals = {
+                'website': obj.reservation_id.warehouse_id.partner_id.website,
+                'city': obj.reservation_id.warehouse_id.partner_id.city,
+                'state_id': obj.reservation_id.warehouse_id.partner_id.state_id,
+                'country_id': obj.reservation_id.warehouse_id.partner_id.country_id,
+                'partner_id': obj.reservation_id.partner_id.name,
+                'partner_street': obj.reservation_id.partner_id.street,
+                'partner_city': obj.reservation_id.partner_id.city,
+                'partner_state_id': obj.reservation_id.partner_id.state_id,
+                'partner_country_id': obj.reservation_id.partner_id.country_id,
+                'partner_phone': obj.reservation_id.partner_id.phone,
+                'amount_total': obj.amount,
+                'fecha_pago': obj.payment_date,
+                'concepto': 'Seña de alojamiento.-',
+            }
+        ret = []
+        ret.append(vals)
+        return ret
+        
