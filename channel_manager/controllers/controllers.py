54da24ba424669bdc56754a87cc7ed9bf0f0a57d
Gabriela Rivero <regaby@gmail.com>
2018-07-04 19:16:39 -0300

    [UPD] logger

--- channel_manager/controllers/controllers.py
+++ channel_manager/controllers/controllers.py
@@ -2,10 +2,13 @@ from openerp import http
 import requests
 import json
 from openerp.http import Response
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class Home(http.Controller):
     @http.route('/test', type='http', auth="none", csrf=False)
     def test(self, **kwargs):
         print kwargs
+        _logger.info(kwargs)
         return Response("TEST",content_type='text/html;charset=utf-8',status=500)
