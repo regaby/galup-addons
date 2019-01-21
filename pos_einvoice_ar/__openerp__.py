aecffe904adc9e9c2010f23778f80c19e4175542
Gabriela Rivero <regaby@gmail.com>
2019-01-21 13:46:57 -0300

    [ADD] pos_einvoice_ar

---
+++ pos_einvoice_ar/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'POS Fields Partner',
+    'version': '0.1',
+    'author': 'Galup Sistemas',
+    'license': 'LGPL-3',
+    'category': 'Point Of Sale',
+    'website': 'www.galup.com.ar',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/pos_einvoice_ar.xml',
+    ],
+    'qweb': [
+        'static/src/xml/pos_payment.xml',
+    ],
+    'installable': True,
+}
