fb21fa1a1ac8aade6633a84b15c525003f0e6d02
Gabriela Rivero <regaby@gmail.com>
2019-09-11 19:27:40 -0300

    [FIX] reserva ruiz ruiz

--- channel_manager/models/channel_manager.py
+++ channel_manager/models/channel_manager.py
@@ -35,8 +35,8 @@ class ChannelManager(models.Model):
 
     @api.multi
     def test2(self):
-        # reservar sode, no entro porque el partner estaba duplicado
-        xml = {'status': u'new', 'reservationid': u'1203198419', 'siteid': u'142', 'site': u'booking_xml', 'propertyid': u'616123'}
+        # reservar ruiz ruiz, no entro por restriccion de cantidad de pasajeros por habitacion
+        xml = {'status': u'new', 'reservationid': u'2310907852', 'siteid': u'142', 'site': u'booking_xml', 'propertyid': u'616123'}
         headers = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'}
         msg = requests.post('http://localhost:8069/test', data=xml, headers=headers).text
         raise UserError(msg)
